3154b1ab13a3a7e9c8febc941c82f11da273f3e1
michellehwin <michellehwin@gmail.com>
2020-09-23 08:01:01 -0500

    commit

--- src/components/SubThemes.vue
+++ src/components/SubThemes.vue
@@ -12,7 +12,7 @@
             imageBack="feather.png"
             imageFront="american-dream.jpg"
             frontImageStart="1"
-            themeParagraph="At the beginning of the movie, June mentions a swan feather that her mother(Suyuan) left for her. This feather was apart of a swan that Suyuan had bought in China that she had try to bring to America; it represented all of the hope she had for her and her daughter. This hope towards the American dream inadvertantly pushed the American culture onto her daughter, making her turn away from her Chinese roots. June's childhood was riddled with conflicts with her mother because she couldn't understand her mother's intentions and Chinese ways. Hope for a better life is what also drove the mothers to survive all of their hardships in China." 
+            themeParagraph="At the beginning of the movie, June mentions a swan feather that her mother(Suyuan) left for her. This feather was apart of a swan that Suyuan had bought in China that she had try to bring to America; it represented all of the hope she had for her and her daughter. This hope towards the American dream inadvertantly pushed the American culture onto her daughter, making her turn away from her Chinese roots. June's childhood was riddled with conflicts with her mother because she couldn't understand her mother's intentions and Chinese ways. Hope for a better life is what also drove the mothers to survive all of their hardships in China. The constant role that hope plays in moving the character's lives forward demonstrates that hope if the driving force of life." 
             imagePaddingTop="20"
         />
         <SubTheme
@@ -27,7 +27,7 @@
             imagePaddingTop="20"
             descriptionPaddingRight="0"
             themeHeading="Sacrifice "
-            themeParagraph="Sacrifice is the ultimate demonstration of love. To come to America and guarantee a good future for their daughters, the mothers had to abandon their home country, something that takes great courage. Both the mothers and daughters also have to sacrifice part of their culture, having to choose between embodying the Chinese or American way in large and small ways, losing something no matter what they pick. The daughters mostly stick to American customs and lose the connection they have to their homeland and their mothers. The mothers mainly stick to Chinese traditions, causing their daughters to be embarassed of them at times."
+            themeParagraph="To come to America and guarantee a good future for their daughters, the mothers had to abandon their home country, something that takes great courage. Both the mothers and daughters also have to sacrifice part of their culture, having to choose between embodying the Chinese or American way in large and small ways, losing something no matter what they pick. The daughters mostly stick to American customs and lose the connection they have to their homeland and their mothers but gain prestiege in the American society. The mothers mainly stick to Chinese traditions, causing their daughters to be embarassed of them at times, but have a better sense of identity. The numerous sacrifices made by the characters both in their daily lives and in monumental moments show that sacrifice is an ultimate gesture of love."
         />
         <SubTheme
             themeHeading="Storytelling "
@@ -47,7 +47,7 @@
             descriptionPaddingLeft="15"
             descriptionPaddingRight="0"
             themeHeading="Hiding Behind a Mask"
-            themeParagraph="People are not always what they seem. Trying to pursure the American dream whilst pleasing their mothers, the daughters put on a happy face, but underneath the mask, they are lost in life. Lena appears happy with Harold, but in reality, she conforms to his liking, doesn't speak up, and doesn't know what she wants in life. Rose is also stuck in a loop of conformity, losing herself to her husband's wishes. The daughters are so caught up with appearing happy and seeming successful that they forget where they came from and their source of happiness in life."
+            themeParagraph="Trying to pursure the American dream whilst pleasing their mothers, the daughters put on a happy face, but underneath the mask, they are lost in life. Lena appears happy with Harold, but in reality, she conforms to his liking, doesn't speak up, and doesn't know what she wants in life. Rose is also stuck in a loop of conformity, losing herself to her husband's wishes. The daughters are so caught up with appearing happy and seeming successful that they forget where they came from and their source of happiness in life. The face they put on when in public and with their families differs greatly from the reality of their situation, showing that people are not always what they seem."
         />
     </b-container>
 </template>
